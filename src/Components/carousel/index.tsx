0e9dcd5b3f6682ebff662776db62a760d0c10862
dev-Beltramin <paulobeltramin.27@gmail.com>
2024-07-10 14:26:01 -0300

    finalizado projeto e iniciando a responsividademobile

--- src/Components/carousel/index.tsx
+++ src/Components/carousel/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { AnimatePresence, motion } from "framer-motion"
 import { heroesData } from "../interfaces/hero"
 import CarouselDetails from "./carouselDetails"
@@ -15,14 +15,48 @@ interface propsDetails {
 }
 
 const Carousel = ({ heroes, activeId }: propsDetails) => {
+
+
   const [isActive, setIsActive] = useState<heroesData[] | null>(null)
   const [heroIndex, setHeroIndex] = useState<number>(
     heroes.findIndex(hero => hero.id === activeId) - 1
   )
 
+
   const handleActiveChange = (newDirection: number) => {
     setHeroIndex((prev) => prev + newDirection)
   }
+
+  const transitionAudio = useMemo(() => new Audio('/songs/transition.mp3'), [])
+
+
+  const voiceHeroes: Record<string, HTMLAudioElement> = useMemo(() => ({
+    "spider-man-616": new Audio('/songs/spider-man-616.mp3'),
+    "mulher-aranha-65": new Audio('/songs/mulher-aranha-65.mp3'),
+    "spider-man-1610": new Audio('/songs/spider-man-1610.mp3'),
+    "sp-dr-14512": new Audio('/songs/sp-dr-14512.mp3'),
+    "spider-ham-8311": new Audio('/songs/spider-ham-8311.mp3'),
+    "spider-man-90214": new Audio('/songs/spider-man-90214.mp3'),
+    "spider-man-928": new Audio('/songs/spider-man-928.mp3')
+  }), [])
+
+
+
+  useEffect(() => {
+
+    if (!isActive) {
+      return;
+    }
+
+    transitionAudio.play()
+
+    const voice = voiceHeroes[isActive[enumPosition.MIDDLE].id]
+
+    voice.volume = 0.3
+    voice.play()
+
+
+  }, [isActive, transitionAudio])
 
   useEffect(() => {
     const visibleHeroes = ((heroIndex % heroes.length) + heroes.length) % heroes.length
@@ -57,12 +91,15 @@ const Carousel = ({ heroes, activeId }: propsDetails) => {
     return null
   }
 
+
   return (
     <main className={styles.container}>
       <div className={styles.carousel}  >
         <AnimatePresence mode="popLayout">
           {Array.isArray(isActive) && isActive.map((item, position) => (
-            <motion.div key={item.id} className={styles.hero} onClick={() => handleActiveChange(1)}
+            <motion.div key={item.id} className={styles.hero}
+              onClick={() => handleActiveChange(1)}
+
               initial={{ x: -1500, scale: "0.75" }}
               animate={{ x: 0, ...carouselModification(position) }}
               transition={{ duration: 0.8 }}
@@ -75,9 +112,9 @@ const Carousel = ({ heroes, activeId }: propsDetails) => {
         </AnimatePresence>
       </div>
 
-      <div className={styles.details}>
+      <motion.div className={styles.details} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 2, delay: 1 }}>
         <CarouselDetails data={isActive[enumPosition.MIDDLE]} />
-      </div>
+      </motion.div>
     </main>
   )
 }
